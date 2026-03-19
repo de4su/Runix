a7e4a4ca20c39d901349cdb0ee0fd9805dd18a46
de4su <de4su@yahoo.com>
2026-03-19 23:42:32 +0100

    defining some variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import Prism from 'prismjs';
+(window as any).Prism = Prism;
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
